2976101c0d015c408fd38ece2d436bbb9437adb6
duhań <duhan2002@gmail.com>
2025-01-02 00:12:25 +0300

    define LCD pins
    * according to their pinouts in the "lcd.c" library

--- sonar-radar.c
+++ sonar-radar.c
@@ -4,5 +4,14 @@
 #use delay(clock=4M)  // 4 MHz osilatör frekansı
 #fuses XT             // Osilatör tipi
 
+// LCD tanımlamalarını yaptım
+#define LCD_RS_PIN PIN_D0
+#define LCD_RW_PIN PIN_D1
+#define LCD_ENABLE_PIN PIN_D2
+#define LCD_DATA4 PIN_D3
+#define LCD_DATA5 PIN_D4
+#define LCD_DATA6 PIN_D5
+#define LCD_DATA7 PIN_D6
+
 // LCD kütüphanesi ekledim
 #include "lcd.c"
