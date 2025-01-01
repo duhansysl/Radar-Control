82181f2b46c9441af4edbe75d60dc65767c32ab8
duhań <duhan2002@gmail.com>
2025-01-02 00:11:04 +0300

    add needed header files
    * PIC16F877A.h
    * lcd.c

---
+++ sonar-radar.c
@@ -0,0 +1,5 @@
+//----------------------- H E A D E R - C O N F I G U R A T I O N  -------------------------------------------------------
+
+#include <16F877A.h>
+// LCD kütüphanesi ekledim
+#include "lcd.c"
