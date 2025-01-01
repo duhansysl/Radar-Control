10d90bf6429b15f69dac93c71cd8b0eca32ca7b2
duhań <duhan2002@gmail.com>
2025-01-02 00:11:55 +0300

    set oscillator mode & frequency
    * XT, 4MHz

--- sonar-radar.c
+++ sonar-radar.c
@@ -1,5 +1,8 @@
 //----------------------- H E A D E R - C O N F I G U R A T I O N  -------------------------------------------------------
 
 #include <16F877A.h>
+#use delay(clock=4M)  // 4 MHz osilatör frekansı
+#fuses XT             // Osilatör tipi
+
 // LCD kütüphanesi ekledim
 #include "lcd.c"
